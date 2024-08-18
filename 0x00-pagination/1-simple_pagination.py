ee3f41ea272cbaf0b9e0397d2fdb0cafa8808322
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-18 14:29:17 +0300

    add file 1-simple_pagination.py

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,37 @@
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """return list of list from dataset based on start and end"""
+        start: int = ((page - 1) * page_size)
+        end: int = start + page_size
+        assert page > 0
+        assert page_size > 0
+        assert isinstance(page, int)
+        assert isinstance(page_size, int)
+        result: List[List[str]] = self.dataset()
+        length = len(result)
+        if (start > length or end > length):
+            return []
+        return result[start:end]
