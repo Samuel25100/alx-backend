b88b80a21e88392e7ded87abdbeffe5b3199ec30
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-20 11:24:55 +0300

    add file 0-basic_cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""""class: BasicCache"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """have put and get method for caching"""
+
+    """def __init__(self):
+        "constructor"
+        super(cache_data)"""
+
+    def put(self, key, item):
+        """puts cached data as key and items in cached_data"""
+        if (key or item):
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """get data from cached_data using key"""
+        if (key):
+            return (self.cache_data.get(key))
