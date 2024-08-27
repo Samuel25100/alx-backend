0987dd7db73e4ad73cd12ed5e5a6f228ec969b18
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-27 14:07:00 +0300

    fix file 0-app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,12 +4,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     """main default route"""
     title = "Welcome to Holberton"
     body = "Hello world"
     return render_template('0-index.html', title=title, body=body)
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000)
