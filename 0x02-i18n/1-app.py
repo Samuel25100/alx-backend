5bbc3ee4847acd7ccc06b39844efdb44c0fb15a0
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-27 14:53:02 +0300

    add files for task-1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""app server for flask"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """config class for Babel"""
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    LANGUAGES = ["en", "fr"]
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """main default route"""
+    title = "Welcome to Holberton"
+    body = "Hello world"
+    return render_template('0-index.html', title=title, body=body)
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000)
