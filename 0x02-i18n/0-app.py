9a2ce293dd4a7723e3d6de28afdb53b8c2e97a56
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-27 14:04:29 +0300

    fix file 0-app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,7 +9,7 @@ def index():
     """main default route"""
     title = "Welcome to Holberton"
     body = "Hello world"
-    return render_template('index.html', title=title, body=body)
+    return render_template('0-index.html', title=title, body=body)
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000)
