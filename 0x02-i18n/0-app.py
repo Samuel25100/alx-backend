9880534a13de59cd84ff669022d6f2b141a50801
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-27 14:02:54 +0300

    add files 0-app.py templates/0-index.html

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""app server for flask"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """main default route"""
+    title = "Welcome to Holberton"
+    body = "Hello world"
+    return render_template('index.html', title=title, body=body)
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000)
