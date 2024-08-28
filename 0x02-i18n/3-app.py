8313294b8e671c87ac3e436b2619fb1892ba665d
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-28 10:07:24 +0300

    fixing files for task-3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """app server for flask"""
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext
+from flask_babel import Babel, gettext as _
 
 
 class Config:
