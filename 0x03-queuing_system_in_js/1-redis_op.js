3ea8073c9166220420b4e6aaa9b33644e22e48a3
Samuel25100 <samuelgselassie1@gmail.com>
2024-09-23 09:43:28 +0300

    add file 1-redis_op.js

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,27 @@
+import { createClient, print } from 'redis';
+
+const client = createClient({
+	host: 'localhost',
+	port: 6379
+});
+client.on('error', (err) => {
+	console.log('Redis client not connected to the server:' + err);
+});
+client.on('ready', () => {
+	console.log('Redis client connected to the server');
+});
+function setNewSchool(schoolName, value) {
+	client.set(schoolName, value, (err, reply) => {
+		print('Reply: ' + reply);
+	});
+}
+
+function displaySchoolValue(schoolName) {
+	client.get(schoolName, (err, reply) => {
+		console.log(reply);
+	});
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
